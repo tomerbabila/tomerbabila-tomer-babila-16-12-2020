0810c47e315c04934e13ebd8f85d0cca965ccdd8
tomertv2 <tomertv2@gmail.com>
2020-12-18 09:46:39 +0200

    Update app.js

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const path = require('path');
-require('dotenv').config()
+require('dotenv').config();
 
 const app = express();
 
