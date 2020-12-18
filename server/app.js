439d037bbbc9e4fd6cb88e880b12cedf8fa83395
tomertv2 <tomertv2@gmail.com>
2020-12-18 09:28:57 +0200

    Run build and serve app from server

--- server/app.js
+++ server/app.js
@@ -1,11 +1,24 @@
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
+const path = require('path');
 
 const app = express();
 
 app.use(express.json());
 app.use(morgan('tiny'));
+app.use(cors());
 
 app.use('/api', require('./api'));
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.use('*', (req, res) => {
+  res.sendStatus(404);
+});
 
 module.exports = app;
